36a8ad6163d5c78d74d3eb3a62ec63a3c4ad2f97
Laura Rico Román <lricrom396@g.educaand.es>
2024-06-11 12:13:07 +0200

    Mi Calculadora básica

---
+++ P_ED_LAURA/src/main/java/com/mycompany/p_ed_laura/Calcular.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.p_ed_laura;
+
+/**
+ *
+ * @author laura
+ */
+
+/**
+ * <h2>Clase con los métodos de la calculadora.</h2>
+ */
+public class Calcular {
+     /**
+     * <p>Constructor por defecto de la clase Calcular.</p>
+     */
+    public Calcular() {
+    }
+
+    /**
+     * <p>Suma dos números enteros.</p>
+     *
+     * @param a Primer número entero.
+     * @param b Segundo número entero.
+     * @return La suma de los dos números enteros.
+     */
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * <p>Resta dos números enteros.</p>
+     *
+     * @param a Primer número entero (minuendo).
+     * @param b Segundo número entero (sustraendo).
+     * @return La resta de los dos números enteros.
+     */
+    public int restar(int a, int b) {
+        return a - b;
+    }
+}
